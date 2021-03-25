0c909d1ec40aeee63fdc4a7403676b1ef1bf4fdd
not4ubut4me <72134710+not4ubut4me@users.noreply.github.com>
2021-03-25 20:07:23 +0300

    Полноэкранный режим для телефонов

--- js/Prims_algorithm.js
+++ js/Prims_algorithm.js
@@ -18,29 +18,9 @@ function Prims_algorithm(){
 
     FindNeighWallsPrims(currPos[0], currPos[1]);
     //-------------------------------------------------------------------------------------------------------
-    build_Prims();
-    function build_Prims()
-    {   
-        var randomInd = Crafty.math.randomInt(0, WallsInProcess.length - 1);
-        VisCells[WallsInProcess[randomInd][0][0]][WallsInProcess[randomInd][0][1]] = true;
-        if (VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] == false)
-        {
-            BreakWall(WallsInProcess[randomInd][0], WallsInProcess[randomInd][1]);
-            FindNeighWallsPrims(WallsInProcess[randomInd][1][0], WallsInProcess[randomInd][1][1]);
-            VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] = true;
-        }
-        WallsInProcess.splice(randomInd, 1);
-        if (WallsInProcess.length != 0)
-        {
-            setTimeout(build_Prims, 0);
-        }
-        
-    }
-    //-------------------------------------------------------------------------------------------------------
-
-
-    // while (WallsInProcess.length != 0)
-    // {
+    // build_Prims();
+    // function build_Prims()
+    // {   
     //     var randomInd = Crafty.math.randomInt(0, WallsInProcess.length - 1);
     //     VisCells[WallsInProcess[randomInd][0][0]][WallsInProcess[randomInd][0][1]] = true;
     //     if (VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] == false)
@@ -50,5 +30,25 @@ function Prims_algorithm(){
     //         VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] = true;
     //     }
     //     WallsInProcess.splice(randomInd, 1);
+    //     if (WallsInProcess.length != 0)
+    //     {
+    //         setTimeout(build_Prims, 0);
+    //     }
+        
     // }
+    //-------------------------------------------------------------------------------------------------------
+
+
+    while (WallsInProcess.length != 0)
+    {
+        var randomInd = Crafty.math.randomInt(0, WallsInProcess.length - 1);
+        VisCells[WallsInProcess[randomInd][0][0]][WallsInProcess[randomInd][0][1]] = true;
+        if (VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] == false)
+        {
+            BreakWall(WallsInProcess[randomInd][0], WallsInProcess[randomInd][1]);
+            FindNeighWallsPrims(WallsInProcess[randomInd][1][0], WallsInProcess[randomInd][1][1]);
+            VisCells[WallsInProcess[randomInd][1][0]][WallsInProcess[randomInd][1][1]] = true;
+        }
+        WallsInProcess.splice(randomInd, 1);
+    }
 }
